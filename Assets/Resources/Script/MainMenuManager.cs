2b184cb34f6021f00d8a8879c71cc30caa8c30cc
Dana Christin <dchristin1817@gmail.com>
2025-10-08 07:27:47 +0700

    feat: Implementasi alur navigasi scene

--- Assets/Resources/Script/MainMenuManager.cs
+++ Assets/Resources/Script/MainMenuManager.cs
@@ -3,10 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Ganti "NamaSceneARAnda" dengan nama scene AR utama Anda yang sebenarnya
-    public string arSceneName = "MainScene";
-
-    // Fungsi publik ini akan kita panggil dari tombol
+    public string arSceneName = "CameraScene";
+    
     public void GoToARScene()
     {
         SceneManager.LoadScene(arSceneName);
--- Assets/Resources/Script/SplashScreenManager.cs
+++ Assets/Resources/Script/SplashScreenManager.cs
@@ -3,22 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreenManager : MonoBehaviour
 {
-    // Berapa lama splash screen akan tampil (dalam detik)
     public float delay = 3f;
-
-    // Nama scene yang akan dimuat setelahnya
     public string sceneToLoad = "MainMenuScene";
 
-    // Fungsi ini dipanggil saat script pertama kali aktif
     void Start()
     {
-        // Panggil fungsi LoadNextScene setelah waktu 'delay'
         Invoke("LoadNextScene", delay);
     }
 
     void LoadNextScene()
     {
-        // Pindah ke scene berikutnya
         SceneManager.LoadScene(sceneToLoad);
     }
 }
